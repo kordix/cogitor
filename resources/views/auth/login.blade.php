2a41814d77ed804edc37953216f3e343a45e5561
= <=>
2019-01-03 20:24:08 +0100

    checked

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" SELECTED> PAMIĘTAJ MNIE
+                                        <input type="checkbox" name="remember" checked> PAMIĘTAJ MNIE
                                     </label>
                                 </div>
                             </div>

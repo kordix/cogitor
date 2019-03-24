f59b2b44876311bafc9e2227031b7758f7b7f23a
= <=>
2019-03-24 10:40:45 +0100

    listshow

--- resources/views/layouts/list.blade.php
+++ resources/views/layouts/list.blade.php
@@ -37,7 +37,7 @@
 <table>
 @foreach($rows as $row)
 <tr>
-    <td class="col-md-3"><a href="{{route('edit', $row->id)}}"> {{$row->question}}</a></td>
+    <td class="col-md-3"><a href="{{route('show', $row->id)}}"> {{$row->question}}</a></td>
     <td class="col-md-2">{{$categories->find($row->category_id)['name']}} </td>
 
     <td class="col-md-1">{{$row->counter}}</td>
